2d095faf2533580b6a9fe80f14c6f9c41d6f3600
vidushirajhans <vidushirajhans@gmail.com>
2025-08-11 22:10:30 +0530

    Fix bugs

--- lib/analyze.py
+++ lib/analyze.py
@@ -13,7 +13,6 @@ def plot_univariate_distributions(df):
     print("Generating Univariate Probability Distributions")
     print("=" * 60 + "\n")
 
-
     for col in df.columns:
         # --- Plotting for NUMERIC columns ---
         if pd.api.types.is_numeric_dtype(df[col]):
